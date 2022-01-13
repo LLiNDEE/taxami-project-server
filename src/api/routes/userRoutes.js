bf2e25cad0a020c35d1817c927aa4f0aa9286a52
LLiNDEE <linus.linde@bahnhof.se>
2022-01-13 16:31:21 +0100

    fix: Add routes user/check/code and admin/generate/code

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import { userJoinBuilding } from "../controllers/userJoinBuilding.js"
 import { newUser } from "../controllers/newUser.js"
 import { userLogin } from "../controllers/userLogin.js"
+import { checkCodeController } from "../controllers/checkCodeController.js"
+import { adminGenerateCode } from "../controllers/adminGenerateCode.js"
 import { validateUserUUID } from "../middlewares/validateUserUUID.js"
 import { checkKeys } from "../middlewares/checkKeys.js"
-import { ROUTES } from '../../utils/constants.js'
+import { checkAdmin } from "../middlewares/checkAdmin.js"
+import { ROUTES } from "../../utils/constants.js"
 
 
 
@@ -16,6 +19,9 @@ const userRoutes = app =>{
     app.post(ROUTES.user_create, checkKeys, newUser)
     app.post(ROUTES.user_login, checkKeys, userLogin)
     app.post(ROUTES.user_building_invite, checkKeys, validateUserUUID, userJoinBuilding)
+    app.post(ROUTES.check_code, checkKeys, checkCodeController)
+
+    app.post([ROUTES.admin_generate_code], checkKeys, checkAdmin, adminGenerateCode)
 
 }
 
