816f25d38cc76a33141924773266b70b62026d1b
LLiNDEE <linus.linde@bahnhof.se>
2022-01-13 16:32:47 +0100

    feat(checkCodeController): Add controller to be able to check if the code is valid and what type of code it is

---
+++ src/api/controllers/checkCodeController.js
@@ -0,0 +1,20 @@
+import { generateError } from "../../utils/errorHandler.js"
+import { generateSuccessResponse } from "../../utils/successHandler.js"
+import { checkCode } from "../models/checkCode.js"
+
+
+export const checkCodeController = async (req, res) => {
+    try{
+
+        const { code } = req.body
+
+        const codeInformationResponse = await checkCode(code)
+        if(!codeInformationResponse.success) return res.status(400).send(generateError('codeError'))
+
+        res.status(200).send(generateSuccessResponse(codeInformationResponse.data))
+        
+    }catch(error){
+        console.log(error)
+        res.status(400).send(generateError())
+    }
+}
--- src/api/models/checkCode.js
+++ src/api/models/checkCode.js
@@ -1,4 +1,4 @@
-import { Code } from './models.js'
+import { Code, Building } from './models.js'
 
 export const checkCode = async code => {
     try{
@@ -11,9 +11,28 @@ export const checkCode = async code => {
             }
         }
 
+        const query = {_id: codeOBJ[0].building_id}
+
+        const building = await Building.findOne(query)
+        if(!building) {
+            return {
+                type: 'noBuilding',
+                success: false
+            }
+        }
+
+        const data = {
+            code_type: codeOBJ[0].type,
+            building: {
+                building_name: building.building_name,
+                _id: building._id
+            }
+        }
+
         return {
             type: 'success',
             success: true,
+            data: data,
         }
 
     }catch(error){
