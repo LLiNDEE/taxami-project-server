b9b3bc398af9c2f3d05204e4f72c28332f01cf4e
LLiNDEE <linus.linde@bahnhof.se>
2022-01-15 13:37:50 +0100

    fix: Update createNewCustomer with if-statements that checks what type of code the user registered with. Depending on if it's an invite/subscription the user will either get the role customer or worker. If it's an invite the user will be added to the buildings member-array

---
+++ src/api/services/createNewCustomer.js
@@ -0,0 +1,68 @@
+import { User } from '../models/models.js'
+import { applyCode } from './applyCode.js'
+import { checkCode } from './checkCode.js'
+
+
+export const createNewCustomer = async ( data ) => {
+    try{
+
+        const checkCodeResponse = await checkCode(data.code)
+        if(!checkCodeResponse.success){
+            return{
+                type: 'codeError',
+                success: false,
+            }
+        }
+
+        const { code_type: type } = checkCodeResponse.data
+
+        if(type === 'subscription'){
+            const user = new User({
+                username: data.username,
+                email: data.email,
+                password: data.password,
+                role: 'customer',
+            })
+    
+            await user.save()
+
+            return {
+                type: 'success',
+                success: true
+            }
+        }
+
+        if(type === 'invite'){
+
+            const user = new User({
+                username: data.username,
+                email: data.email,
+                password: data.password
+            })
+
+            await user.save()
+
+            const user_id = user._id
+
+            const { code: invite_code } = data
+
+            const applyCodeResponse = await applyCode(invite_code, user_id)
+            if(!applyCodeResponse.success) return res.status(400).send(generateError(applyCodeResponse.type))
+
+            return {
+                type: 'success',
+                success: true,
+            }
+
+        }
+
+
+
+    }catch(error){
+        console.log(error)
+        return {
+            type: 'failedToCreate',
+            success: false,
+        }
+    }
+}
