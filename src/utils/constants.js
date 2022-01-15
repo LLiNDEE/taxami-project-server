ea8a26af654b42381295c7c78b8f511e711f8309
LLiNDEE <linus.linde@bahnhof.se>
2022-01-15 23:33:05 +0100

    fix: Add TASK_PRIORITY_TYPES as a constant with the values low, medium and high. May be updated in the future

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,6 +23,12 @@ export const TASK_STATUS_TYPES = {
     public: 'public',
 }
 
+export const TASK_PRIORITY_TYPES = {
+    low: 'low',
+    medium: 'medium',
+    high: 'high',
+}
+
 export const ERROR_TYPES = {
     invalidParams: 'invalid_params',
     invalidPassword: 'invalidPASsword',
@@ -34,6 +40,7 @@ export const ERROR_TYPES = {
     failedToApplyInvite: 'failedToApplyInvite',
     failedToApplyCode: 'failedToApplyCode',
     failedToAddBuilding: 'failedToAddBuilding',
+    failedToAddTask: 'failedToAddTask',
     failedToCreateInvite: 'failedToCreateInvite',
     failedToGenerateCode: 'failedToGenerateCode',
     noBuilding: 'noBuilding',
@@ -54,6 +61,7 @@ export const ROUTES = {
     user_building_invite: '/user/join',
     check_code: '/user/check/code',
     building_create: '/building/create',
+    building_add_task: '/building/add/task',
     building_invite: '/building/generate/invite',
 }
 
@@ -79,6 +87,14 @@ const CREATE_BUILDING_KEYS = {
     building_name: 'building_name',
 }
 
+const BUILDING_ADD_TASK_KEYS = {
+    user_id: 'user_id',
+    building_id: 'building_id',
+    title: 'title',
+    description: 'description',
+    priority: 'priority',
+}
+
 const USER_JOIN_INVITE = {
     user_id: 'user_id',
     invite_code: 'invite_code',
@@ -102,6 +118,7 @@ export const PATH_KEYS = {
     [ROUTES.user_create]: USER_CREATE_KEYS,
     [ROUTES.building_create]: CREATE_BUILDING_KEYS,
     [ROUTES.building_invite]: CREATE_INVITE_KEYS,
+    [ROUTES.building_add_task]: BUILDING_ADD_TASK_KEYS,
     [ROUTES.user_building_invite]: USER_JOIN_INVITE,
     [ROUTES.check_code]: CHECK_CODE_KEYS,
     [ROUTES.admin_generate_code]: ADMIN_GENERATE_CODE_KEYS,
