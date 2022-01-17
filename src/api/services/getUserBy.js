a7bf1c3628ca4cb64377d8dac67571d8b99a4d5b
Linus Linde <linus.linde@bahnhof.se>
2022-01-17 08:28:52 +0100

    fix: Update function with new argument 'config'. By default it will get everything about the user except the password. But if you use the config-arg and send it in as 'password' it will get the password aswell

--- src/api/services/getUserBy.js
+++ src/api/services/getUserBy.js
@@ -1,13 +1,30 @@
 import { User } from '../models/models.js'
 
-export const getUserBy = async (key = 'username', data) => {
+export const getUserBy = async (key = 'username', data, config) => {
     try{
+
+        if(config === 'password'){
+            const userResponse = await User.find({ [key]: data }).select('password')
+            if(!userResponse || userResponse.length < 1){
+                return {
+                    type: 'noUser',
+                    success: false,
+                }
+            }
+
+            return {
+                type: 'userExist',
+                user: userResponse,
+                success: true,
+            }
+
+        }
 
         const userResponse = await User.find({ [key]: data})
         if(!userResponse || userResponse.length < 1){
             return {
                 type: 'noUser',
-                success: true,
+                success: false,
             }
         }
 
