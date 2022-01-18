811454e4ab96cf4f3415590c1e3f8011f74042c9
Linus Linde <linus.linde@bahnhof.se>
2022-01-18 10:56:29 +0100

    fix: Add route /user/leave/task

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -3,9 +3,10 @@ import { newUser } from "../controllers/newUser.js"
 import { userLogin } from "../controllers/userLogin.js"
 import { checkCodeController } from "../controllers/checkCodeController.js"
 import { adminGenerateCode } from "../controllers/adminGenerateCode.js"
-import { userTakeTaskController } from '../controllers/userTakeTaskController.js'
+import { userLeaveTask } from "../controllers/userLeaveTask.js"
+import { userTakeTaskController as userTakeTask } from '../controllers/userTakeTaskController.js'
 import { userCompleteTaskController as userCompleteTask } from "../controllers/userCompleteTaskController.js"
-import { getAllCustomersController } from "../controllers/getAllCustomersController.js"
+import { getAllCustomersController as getAllCustomers } from "../controllers/getAllCustomersController.js"
 import { validateUserUUID } from "../middlewares/validateUserUUID.js"
 import { checkKeys } from "../middlewares/checkKeys.js"
 import { checkAdmin } from "../middlewares/checkAdmin.js"
@@ -24,11 +25,12 @@ const userRoutes = app =>{
     app.post(ROUTES.user_building_invite, checkKeys, validateUserUUID, userJoinBuilding)
     app.post(ROUTES.check_code, checkKeys, checkCodeController)
 
-    app.post([ROUTES.user_take_task], checkKeys, validateUserUUID, userTakeTaskController)
+    app.post([ROUTES.user_take_task], checkKeys, validateUserUUID, userTakeTask)
     app.post([ROUTES.user_complete_task], checkKeys, validateUserUUID, userCompleteTask)
+    app.post([ROUTES.user_leave_task], checkKeys, validateUserUUID, userLeaveTask)
 
     app.post([ROUTES.admin_generate_code], checkKeys, checkAdmin, adminGenerateCode)
-    app.post([ROUTES.admin_get_customers], checkKeys, checkAdmin, getAllCustomersController)
+    app.post([ROUTES.admin_get_customers], checkKeys, checkAdmin, getAllCustomers)
 
 }
 
