a7c887ad370603e0592e645e49ef9ed5c720ae6d
Linus Linde <linus.linde@bahnhof.se>
2022-01-18 15:19:39 +0100

    fix: Add route/controller/route for updating task information.

---
+++ src/api/controllers/updateTask.js
@@ -0,0 +1,35 @@
+import { generateError } from "../../utils/errorHandler.js"
+import { generateSuccessResponse } from "../../utils/successHandler.js"
+import { updateTaskData } from "../services/updateTaskData.js"
+import { filterValidKeys } from "../validations/validateKeys.js"
+
+const TASK_VALID_KEYS = {
+    title: 'title',
+    description: 'description',
+    priority: 'priority',
+    status: 'status',
+}
+
+export const updateTask = async (req, res) => {
+    try{
+
+        const {user_id, task_id, ...DATA} = req.body
+
+        const validKeys = filterValidKeys(DATA, TASK_VALID_KEYS)
+
+        // if(validKeys.length === 0) return res.status(400).send(generateError('error'))
+
+        const formattedKeys = formatValidKeys(validKeys, DATA)
+
+        const updateTaskResponse = await updateTaskData(user_id, task_id, formattedKeys)
+        if(!updateTaskResponse.success) return res.status(400).send(generateError(updateTaskResponse.type))
+
+        res.status(200).send(generateSuccessResponse("", {message: "Task successfully updated"}))
+
+    }catch(error){
+        console.log(error)
+        res.status(400).send(generateError('error'))
+    }
+}
+
+const formatValidKeys = (keys, values) => keys.reduce((p, k) =>( p[k] = values[k], p ), {})
---
+++ src/api/services/updateTaskData.js
@@ -0,0 +1,40 @@
+import { Task } from '../models/models.js'
+import { getUserBy } from './getUserBy.js'
+
+
+
+
+export const updateTaskData = async (user_id, task_id, values) => {
+    try{
+
+        const userOBJ = await getUserBy('_id', user_id)
+        if(!userOBJ.success || userOBJ.type === 'noUser'){
+            return {
+                type: 'failed',
+                success: false
+            }
+        }
+
+        const userTasks = userOBJ.user[0].tasks
+        if(!userTasks.includes(task_id)) {
+            return {
+                type: 'noPermission',
+                success: false
+            }
+        }
+
+        const taskQuery = {_id: task_id}
+        await Task.findOneAndUpdate(taskQuery, {$set: values})
+
+        return {
+            type: 'success',
+            success: true,
+        }
+
+    }catch(error){
+        return {
+            type: 'error',
+            success: false,
+        }
+    }
+}
