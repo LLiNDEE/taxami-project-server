360653396fa06c1714f38409a57bf631781ab059
Linus Linde <linus.linde@bahnhof.se>
2022-01-18 11:25:17 +0100

    fix: Fix so only users with the role customer can create a new building

--- src/api/controllers/newBuilding.js
+++ src/api/controllers/newBuilding.js
@@ -2,17 +2,21 @@ import { generateError } from "../../utils/errorHandler.js";
 import { generateSuccessResponse } from "../../utils/successHandler.js";
 import { createNewBuilding } from "../services/createNewBuilding.js";
 import { addBuildingToUser } from "../services/addBuildingToUser.js";
+import { getUserBy } from "../services/getUserBy.js";
 
 export const newBuilding = async (req, res) => {
     try{
 
         const DETAILS = req.body
+        const { user_id } = req.body
+
+        const userOBJ = await getUserBy('_id', user_id)
+        if(userOBJ.user[0].role !== 'customer') return res.status(200).send(generateError('noPermission'))
 
         const addBuildingResponse = await createNewBuilding(DETAILS)
         if(!addBuildingResponse.success) return res.status(400).send(generateError(addBuildingResponse.type))
 
         const building_id = addBuildingResponse?.building_id
-        const { user_id } = req.body
 
         const addBuildingToUserResponse = await addBuildingToUser(user_id, building_id)
         if(!addBuildingToUserResponse.success) return generateError('error', {message: "Could not add building to user..."})
