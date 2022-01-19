281b01cd153b8c3508ee559714c01154987408b0
Linus Linde <linus.linde@bahnhof.se>
2022-01-19 09:50:11 +0100

    fix: Implement the route for removing a member from a building

--- src/api/routes/buildingRoutes.js
+++ src/api/routes/buildingRoutes.js
@@ -2,6 +2,7 @@ import { newBuilding } from "../controllers/newBuilding.js"
 import { newTask } from "../controllers/newTask.js"
 import { updateTask } from "../controllers/updateTask.js"
 import { newInvite } from "../controllers/newInvite.js"
+import { removeMember } from "../controllers/removeMember.js"
 import { validateUserUUID } from "../middlewares/validateUserUUID.js"
 import { checkKeys } from "../middlewares/checkKeys.js"
 import { ROUTES } from "../../utils/constants.js"
@@ -13,6 +14,8 @@ const buildingRoutes = app =>{
     app.post(ROUTES.building_invite, checkKeys, validateUserUUID, newInvite)
     app.post(ROUTES.building_add_task, checkKeys, validateUserUUID, newTask)
     app.post(ROUTES.building_update_task, checkKeys, validateUserUUID, updateTask)
+
+    app.post(ROUTES.building_remove_member, checkKeys, validateUserUUID, removeMember)
 
 }
 
