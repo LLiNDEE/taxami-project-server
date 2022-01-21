d14f42a39d72d3d9ce4bbd8ab1cb81052d3f643b
Linus Linde <linus.linde@bahnhof.se>
2022-01-21 10:21:33 +0100

    fix: Add service that gets the buildings the user is a member of

---
+++ src/api/services/getUserBuildings.js
@@ -0,0 +1,31 @@
+import { Building } from "../models/models.js"
+
+
+
+
+export const getUserBuildings = async user_id => {
+    try{
+
+        const buildingQuery = {members: user_id}
+        const buildings = await Building.find(buildingQuery)
+        if(!buildings || buildings.length < 1) {
+            return {
+                type: 'noBuilding',
+                success: false
+            }
+        }
+
+        return {
+            type: 'success',
+            success: true,
+            buildings: buildings
+        }
+
+    }catch(error){
+        console.log(error)
+        return {
+            type: 'error',
+            success: false
+        }
+    }
+}
