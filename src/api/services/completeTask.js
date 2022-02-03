1cee9bd4695dcdb64482b4739fdc981af35ea799
Linus Linde <linus.linde@bahnhof.se>
2022-02-03 10:54:44 +0100

    fix: Add extra check for the task_id in the users completed_tasks. This is because of the update taks function

--- src/api/services/completeTask.js
+++ src/api/services/completeTask.js
@@ -21,8 +21,9 @@ export const completeTask = async data => {
         const task = await Task.findOne(taskQuery)
 
         const userTasks = userOBJ.user[0].tasks
+        const userCompletedTasks = userOBJ.user[0].completed_tasks
 
-        if(!userTasks.includes(task_id) && user_id !== task.user_id) {
+        if(!userTasks.includes(task_id) && !userCompletedTasks.includes(task_id) && user_id !== task.user_id) {
             return {
                 type: 'noPermission',
                 success: false
