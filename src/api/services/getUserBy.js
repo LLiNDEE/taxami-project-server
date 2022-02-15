fbd4d47fe24e5ea85b28638a36f34d7958fbab8a
Linus Linde <linus.linde@bahnhof.se>
2022-02-15 11:18:32 +0100

    fix: Fix bug that only allowed customers/workers to login. Fix it by checking inside the admin-collection aswell and adding some extra if statements

--- src/api/services/getUserBy.js
+++ src/api/services/getUserBy.js
@@ -1,11 +1,12 @@
-import { User } from '../models/models.js'
+import { User, Admin } from '../models/models.js'
 
 export const getUserBy = async (key = 'username', data, config = "") => {
     try{
 
         if(config === 'password'){
             const userResponse = await User.find({ [key]: data }).select('password')
-            if(!userResponse || userResponse.length < 1){
+            const adminResponse = await Admin.find({ [key]: data }).select('password')
+            if((!userResponse || userResponse.length < 1) && (!adminResponse || adminResponse.length < 1)){
                 return {
                     type: 'noUser',
                     success: false,
@@ -14,14 +15,15 @@ export const getUserBy = async (key = 'username', data, config = "") => {
 
             return {
                 type: 'userExist',
-                user: userResponse,
+                user: userResponse.length < 1 ? adminResponse : userResponse,
                 success: true,
             }
 
         }
 
         const userResponse = await User.find({ [key]: data})
-        if(!userResponse || userResponse.length < 1){
+        const adminResponse = await Admin.find({ [key]: data })
+        if((!userResponse || userResponse.length < 1) && (!adminResponse || adminResponse.length < 1)){
             return {
                 type: 'noUser',
                 success: false,
@@ -30,7 +32,7 @@ export const getUserBy = async (key = 'username', data, config = "") => {
 
         return {
             type: 'userExist',
-            user: userResponse,
+            user: userResponse.length < 1 ? adminResponse : userResponse,
             success: true,
         }
 
