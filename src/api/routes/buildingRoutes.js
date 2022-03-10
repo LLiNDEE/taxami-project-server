bda56a17059f75c339a6add3df03cfa527783528
LLiNDEE <linus.linde@bahnhof.se>
2022-03-10 20:18:25 +0100

    fix: Add routes /building/remove/task and /building/add/permissions

--- src/api/routes/buildingRoutes.js
+++ src/api/routes/buildingRoutes.js
@@ -6,25 +6,31 @@ import { removeMember } from "../controllers/removeMember.js"
 import { deleteBuilding } from '../controllers/deleteBuilding.js'
 import { validateUserUUID } from "../middlewares/validateUserUUID.js"
 import { checkKeys } from "../middlewares/checkKeys.js"
+import { deleteTask } from '../controllers/deleteTask.js'
 import { validateToken } from '../middlewares/validateToken.js'
 import { ROUTES } from "../../utils/constants.js"
 import { removeTask } from "../controllers/removeTask.js"
 import { buildingGetTasks } from "../controllers/buildingGetTasks.js"
 import { getMembers } from "../controllers/getMembers.js"
+import { BuildingAddPermissions } from "../controllers/BuildingAddPermissions.js"
 
 const buildingRoutes = app =>{
 
     app.post(ROUTES.building_create, validateToken, validateUserUUID, newBuilding)
     app.post(ROUTES.building_invite, validateToken, validateUserUUID, newInvite)
-    app.post(ROUTES.building_add_task, validateToken, validateUserUUID, newTask)
-    app.post(ROUTES.building_update_task, validateToken, validateUserUUID, updateTask)
 
+    app.post(ROUTES.building_add_task, validateToken, validateUserUUID, newTask)
+    app.post('/building/remove/task', validateToken, deleteTask)
+    app.post(ROUTES.building_update_task, validateToken, validateUserUUID, updateTask)
+    
     app.post(ROUTES.building_get_tasks, validateToken, validateUserUUID, buildingGetTasks)
     app.post(ROUTES.building_get_members, validateToken, validateUserUUID, getMembers)
 
     app.post(ROUTES.building_remove_member, validateToken, validateUserUUID, removeMember)
     app.post(ROUTES.building_delete, validateToken, validateUserUUID, deleteBuilding)
     app.post(ROUTES.building_remove_task, validateToken, validateUserUUID, removeTask)
+
+    app.post('/building/add/permissions', BuildingAddPermissions)
 
 }
 
